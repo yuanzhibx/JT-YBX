6b8ec20cf8adf45e5a8753ab02cf13811dde1284
yuanzhibx_glb <ybx0729@163.com>
2020-07-04 14:46:27 +0800

    7 月 4 日 yuanzhibx add demo02(Mybatis)

---
+++ springboot_demo02/src/main/java/com/jt/SpringbootDemo02Application.java
@@ -0,0 +1,16 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuanzhibx
+ */
+@SpringBootApplication
+public class SpringbootDemo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo02Application.class, args);
+    }
+
+}
